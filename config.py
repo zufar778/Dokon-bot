f70d8a871c3d75166be1989280b29acb195708b5
zufar778 <zarifboyevzufarbek06@gmail.com>
2025-10-23 17:05:18 +0500

    Add files via upload
    State Bot

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-bot_token = "8401490747:AAG14dsIUwR4Knp5RZnoiCl8nGtRTu2-gz8"
-admins = [1740889690]
+Bot_token = "8307819519:AAEVJ6TRxIGPenDvmTFxAQbj4PYn1J6Do1Q"
+kanal_id = -1002361009102
--- states.py
+++ states.py
@@ -1,13 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class Product(StatesGroup):
-    all = State()
-    go = State()
-    yes = State()
-    no = State()
-
-
-class Karzinka(StatesGroup):
-    contact = State()
-    location = State()
-    confirmation = State()
+class Step(StatesGroup):
+    activity = State()
+    language = State()
+    send = State()
